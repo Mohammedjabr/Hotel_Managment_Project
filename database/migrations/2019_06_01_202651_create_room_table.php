235fb49ee93f32dfe1a33047b7b13cf57f2de243
Mohammed shmala <mhmdjaber120161209@gmail.com>
2019-06-23 10:06:57 +0300

    migration

---
+++ database/migrations/2019_06_01_202651_create_room_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('room_number');
+            $table->enum('type', ['single', 'double', 'king', 'twin', 'cabana', 'superior', 'junior_suite']);//open website to show what there like booking.com
+            $table->integer('price');
+            $table->enum('book_type', ['daily', 'monthly']);
+            $table->boolean('is_booked');
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room');
+    }
+}
---
+++ database/migrations/2019_06_01_203138_create_hotel_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHotelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('hotel_stars');//enum
+            $table->integer('phone_number');
+            $table->string('address');
+            $table->integer('number_of_rooms');
+            $table->text('hotel_services');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotel');
+    }
+}
